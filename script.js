cf248655f80ee5d9f895cf208b32b0a3cf98b27c
tuannguyenfu <tuannguyenfu@gmail.com>
2020-11-26 10:09:37 +0700

    done

--- script.js
+++ script.js
@@ -3,12 +3,13 @@ var isHidden = "hidden";
 var btnNext = document.getElementById("next");
 
 function welcome() {
-  document.getElementById("next").style.display = "none";
   sayHello();
+  document.getElementById("next").style.display = "block";
 }
 
 function sayHello() {
   alert("Welcome to the first JavaScript!");
+  setInterval ( inputName(), 1000 );
 }
 
 function sayGoodbye() {
@@ -18,14 +19,17 @@ function sayGoodbye() {
 function inputName() {
   var name = prompt("Please enter your name");
   document.getElementById("name").innerHTML = "Full Name: <b>" + name + "</b>";
+  inputCity();
 }
 
 function inputCity() {
   var city = prompt("Please enter your city");
   document.getElementById("city").innerHTML = "City: <b>" + city + "</b>";
+  inputPhone();
 }
 
 function inputPhone() {
   var phone = prompt("Please enter your phone number");
   document.getElementById("mobile").innerHTML = "Mobile: <b>" + phone + "</b>";
+  sayGoodbye();
 }
